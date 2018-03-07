ffb6008846b6c0dc8a8165f67bd380d64df7ed85
Venderlich <tristan.cuenot@laposte.net>
2018-03-07 17:14:04 +0100

    Supression des spinner pour ajouter des Texfield Double

--- src/journal/TypeDeCour/GUI/CreateTypeDeCourController.java
+++ src/journal/TypeDeCour/GUI/CreateTypeDeCourController.java
@@ -1,31 +1,48 @@
 package journal.TypeDeCour.GUI;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
 import journal.TypeDeCour.Service.CreateTypeDeCour;
+
+import javax.xml.soap.Text;
+import java.net.URL;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 
-public class CreateTypeDeCourController {
+public class CreateTypeDeCourController implements Initializable{
 
     @FXML
     private TextField txtTDC;
 
     @FXML
-    private Spinner<Double> spnRapport;
+    private TextField txtRapport;
 
     @FXML
     private Label lblInfo;
 
     CreateTypeDeCour creator = new CreateTypeDeCour();
 
-
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        txtRapport.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (!newValue.matches("\\d{0,7}([\\.]\\d{0,4})?")) {
+                    txtRapport.setText(oldValue);
+                }
+            }
+        });
+    }
     public void createTypeDeCour(ActionEvent event){
         try {
-            creator.CreateTypeDeCours(txtTDC.getText(),spnRapport.getValue());
+            creator.CreateTypeDeCours(txtTDC.getText(),Double.parseDouble(txtRapport.getText()));
             lblInfo.setText("Vous avez inserez la filiére " + txtTDC.getText());
         } catch (SQLException e) {
             e.printStackTrace();
