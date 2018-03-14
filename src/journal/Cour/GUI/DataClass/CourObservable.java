fe5423a53b57607772607b73601e1a9d503945f6
Venderlich <tristan.cuenot@laposte.net>
2018-03-14 15:51:56 +0100

    ajout d'un tree table wiew dans la GUI de cour

---
+++ src/journal/Cour/GUI/DataClass/CourObservable.java
@@ -0,0 +1,79 @@
+package journal.Cour.GUI.DataClass;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class CourObservable {
+    private SimpleStringProperty id;
+    private SimpleStringProperty date;
+    private SimpleStringProperty temps;
+    private SimpleStringProperty filiere;
+    private SimpleStringProperty typeDeCour;
+    private SimpleStringProperty matiere;
+    private SimpleStringProperty commentaire;
+
+    public CourObservable(String id, String date, String temps, String filiere, String typeDeCour, String matiere, String commentaire){
+        this.id =new SimpleStringProperty(id);
+        this.date= new SimpleStringProperty(date);
+        this.temps= new SimpleStringProperty(temps);
+        this.filiere= new SimpleStringProperty(filiere);
+        this.typeDeCour= new SimpleStringProperty(typeDeCour);
+        this.matiere= new SimpleStringProperty(matiere);
+        this.commentaire= new SimpleStringProperty(commentaire);
+    }
+
+    public String getId(){
+        return id.get();
+    }
+
+    public void setId(String id){
+        this.id.set(id);
+    }
+
+    public String getDate(){
+        return date.get();
+    }
+
+    public void setDate(String date){
+        this.date.set(date);
+    }
+
+    public String getTemps(){
+        return temps.get();
+    }
+
+    public void setTemps(String temps){
+        this.temps.set(temps);
+    }
+
+    public String getFiliere(){
+        return filiere.get();
+    }
+
+    public void setFiliere(String filiere){
+        this.filiere.set(filiere);
+    }
+
+    public String getTypeDeCour(){
+        return typeDeCour.get();
+    }
+
+    public void setTypeDeCour(String typeDeCour){
+        this.typeDeCour.set(typeDeCour);
+    }
+
+    public String getMatiere(){
+        return matiere.get();
+    }
+
+    public void setMatiere(String matiere){
+        this.matiere.set(matiere);
+    }
+
+    public String getCommentaire(){
+        return commentaire.get();
+    }
+
+    public void setCommentaire(String commentaire){
+        this.commentaire.set(commentaire);
+    }
+}
--- src/journal/Cour/GUI/ReadCourController.java
+++ src/journal/Cour/GUI/ReadCourController.java
@@ -1,37 +1,74 @@
 package journal.Cour.GUI;
 
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
+import journal.Cour.GUI.DataClass.CourObservable;
+
 import journal.Cour.Service.ReadCour;
 
 import java.net.URL;
 import java.sql.SQLException;
+
 import java.util.ResourceBundle;
+
+import static javafx.collections.FXCollections.observableArrayList;
 
 public class ReadCourController implements Initializable {
 
     @FXML
-    private ListView<String> courListView;
+    private TableView<CourObservable> courListView;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         ReadCour lector = new ReadCour();
-        ObservableList<String> cour = courListView.getItems();
-        int i = 0;
-        try{
-            while ( i < lector.listCour().size()){
-                cour.add(lector.listCour().get(i).getCommentaire());
-                cour.add(lector.listCour().get(i).getDateSeance());
-                cour.add(lector.listCour().get(i).getTemps().toString());
-                cour.add(lector.listCour().get(i).getMatiere().getLabel());
-                cour.add(lector.listCour().get(i).getFiliere().getLabel());
-                cour.add(lector.listCour().get(i).getTypeDeCour().getLabel());
+
+        TableColumn dateCol = new TableColumn("DATE");
+        TableColumn tempsCol = new TableColumn("TEMPS");
+        TableColumn filiereCol = new TableColumn("FILIERE");
+        TableColumn tdcCol = new TableColumn("COUR");
+        TableColumn matiereCol = new TableColumn("MATIERE");
+        TableColumn commentaireCol = new TableColumn("COMMENTAIRE");
+        courListView.getColumns().addAll(dateCol,tempsCol,filiereCol,tdcCol,matiereCol,commentaireCol);
 
 
-                i = i + 1;}
-        } catch (SQLException e){
 
+        ObservableList<CourObservable> cour = observableArrayList();{
+            int i = 0;
+            try{
+                while ( i < lector.listCour().size()){
+                   cour.add(new CourObservable(lector.listCour().get(i).getId().toString(),
+                            lector.listCour().get(i).getDateSeance(),
+                            lector.listCour().get(i).getTemps().toString(),
+                            lector.listCour().get(i).getFiliere().getLabel(),
+                            lector.listCour().get(i).getTypeDeCour().getLabel(),
+                            lector.listCour().get(i).getMatiere().getLabel(),
+                            lector.listCour().get(i).getCommentaire()
+                            ));
+                    i = i + 1;}
+            } catch (SQLException e){
+
+            }
         }
+        courListView.setItems(cour);
+
+
+        dateCol.setCellValueFactory(new PropertyValueFactory<CourObservable,String>("date"));
+        tempsCol.setCellValueFactory(new PropertyValueFactory<CourObservable,String>("temps"));
+        filiereCol.setCellValueFactory(new PropertyValueFactory<CourObservable,String>("filiere"));
+        tdcCol.setCellValueFactory(new PropertyValueFactory<CourObservable,String>("typeDeCour"));
+        matiereCol.setCellValueFactory(new PropertyValueFactory<CourObservable,String>("matiere"));
+        commentaireCol.setCellValueFactory(new PropertyValueFactory<CourObservable,String>("commentaire"));
+
+
+
     }
 }
